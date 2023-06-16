226bb6d90c30852f02648195355d6920f4ce1905
Daniel Irwin <danielirwin99@gmail.com>
2023-06-16 12:06:19 +1000

    Added certs and other packages

--- API/Program.cs
+++ API/Program.cs
@@ -12,17 +12,22 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// This allows our browser to accept our localhost:5001 as trustworthy for the Angular requests from it
+
 // Anything before var app is considered our SERVICES CONTAINER
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. --> AS THE REQUEST COMES INTO OUR PIPELINE
 
 // MIDDLEWARE
 app.UseHttpsRedirection();
 
-// app.UseAuthorization();
+// Configure the HTTP request pipeline. --> AS THE REQUEST COMES INTO OUR PIPELINE
+// Allowing us to connect to our FRONTEND application
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
 app.Run();
+// app.UseAuthorization(); --> Not using it ATM
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // PROPERTIES
+  // -----------
   title = 'MatchMates';
+  users: any;
+
+  // Constructor
+  // ------------
+  // We want to inject our HTTP client into this component NOT the module
+  // What we are going to use in here is only going to be available inside this class --> Private
+  constructor(private http: HttpClient) {}
+
+  // METHOD
+  // ---------------
+  // Getting the API --> subscribe so it does more than observing and gets it
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      // What we want to do with the response back
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request completed'),
+    });
+  }
 }
